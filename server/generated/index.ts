90f90a0c3f0a783a55403e11b236ec1ff4b94d52
Ricky Gonzales <gonzalrick@gmail.com>
2019-02-17 17:39:18 +1000

    Added gql-gen

---
+++ server/generated/index.ts
@@ -0,0 +1,143 @@
+export type Maybe<T> = T | null;
+
+// ====================================================
+// Types
+// ====================================================
+
+export interface Query {
+  schedule: (Maybe<Schedule>)[];
+}
+
+export interface Schedule {
+  gameId: string;
+}
+
+// ====================================================
+// Arguments
+// ====================================================
+
+export interface ScheduleQueryArgs {
+  date: string;
+}
+
+import { GraphQLResolveInfo } from 'graphql';
+
+export type Resolver<Result, Parent = {}, Context = {}, Args = {}> = (
+  parent: Parent,
+  args: Args,
+  context: Context,
+  info: GraphQLResolveInfo,
+) => Promise<Result> | Result;
+
+export interface ISubscriptionResolverObject<Result, Parent, Context, Args> {
+  subscribe<R = Result, P = Parent>(
+    parent: P,
+    args: Args,
+    context: Context,
+    info: GraphQLResolveInfo,
+  ): AsyncIterator<R | Result> | Promise<AsyncIterator<R | Result>>;
+  resolve?<R = Result, P = Parent>(
+    parent: P,
+    args: Args,
+    context: Context,
+    info: GraphQLResolveInfo,
+  ): R | Result | Promise<R | Result>;
+}
+
+export type SubscriptionResolver<
+  Result,
+  Parent = {},
+  Context = {},
+  Args = {}
+> =
+  | ((
+      ...args: any[]
+    ) => ISubscriptionResolverObject<Result, Parent, Context, Args>)
+  | ISubscriptionResolverObject<Result, Parent, Context, Args>;
+
+export type TypeResolveFn<Types, Parent = {}, Context = {}> = (
+  parent: Parent,
+  context: Context,
+  info: GraphQLResolveInfo,
+) => Maybe<Types>;
+
+export type NextResolverFn<T> = () => Promise<T>;
+
+export type DirectiveResolverFn<TResult, TArgs = {}, TContext = {}> = (
+  next: NextResolverFn<TResult>,
+  source: any,
+  args: TArgs,
+  context: TContext,
+  info: GraphQLResolveInfo,
+) => TResult | Promise<TResult>;
+
+export namespace QueryResolvers {
+  export interface Resolvers<Context = {}, TypeParent = {}> {
+    schedule?: ScheduleResolver<(Maybe<Schedule>)[], TypeParent, Context>;
+  }
+
+  export type ScheduleResolver<
+    R = (Maybe<Schedule>)[],
+    Parent = {},
+    Context = {}
+  > = Resolver<R, Parent, Context, ScheduleArgs>;
+  export interface ScheduleArgs {
+    date: string;
+  }
+}
+
+export namespace ScheduleResolvers {
+  export interface Resolvers<Context = {}, TypeParent = Schedule> {
+    gameId?: GameIdResolver<string, TypeParent, Context>;
+  }
+
+  export type GameIdResolver<
+    R = string,
+    Parent = Schedule,
+    Context = {}
+  > = Resolver<R, Parent, Context>;
+}
+
+/** Directs the executor to skip this field or fragment when the `if` argument is true. */
+export type SkipDirectiveResolver<Result> = DirectiveResolverFn<
+  Result,
+  SkipDirectiveArgs,
+  {}
+>;
+export interface SkipDirectiveArgs {
+  /** Skipped when true. */
+  if: boolean;
+}
+
+/** Directs the executor to include this field or fragment only when the `if` argument is true. */
+export type IncludeDirectiveResolver<Result> = DirectiveResolverFn<
+  Result,
+  IncludeDirectiveArgs,
+  {}
+>;
+export interface IncludeDirectiveArgs {
+  /** Included when true. */
+  if: boolean;
+}
+
+/** Marks an element of a GraphQL schema as no longer supported. */
+export type DeprecatedDirectiveResolver<Result> = DirectiveResolverFn<
+  Result,
+  DeprecatedDirectiveArgs,
+  {}
+>;
+export interface DeprecatedDirectiveArgs {
+  /** Explains why this element was deprecated, usually also including a suggestion for how to access supported similar data. Formatted using the Markdown syntax (as specified by [CommonMark](https://commonmark.org/). */
+  reason?: string;
+}
+
+export interface IResolvers<Context = {}> {
+  Query?: QueryResolvers.Resolvers<Context>;
+  Schedule?: ScheduleResolvers.Resolvers<Context>;
+}
+
+export interface IDirectiveResolvers<Result> {
+  skip?: SkipDirectiveResolver<Result>;
+  include?: IncludeDirectiveResolver<Result>;
+  deprecated?: DeprecatedDirectiveResolver<Result>;
+}
