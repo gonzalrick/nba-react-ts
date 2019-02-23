ca8ba4657832d040044b510851c822c87f689317
Ricky Gonzales <gonzalrick@gmail.com>
2019-02-23 23:58:20 +1000

    schedule type

--- server/app/schema/schedule/schedule.type.ts
+++ server/app/schema/schedule/schedule.type.ts
@@ -3,4 +3,14 @@ import { Schedule as ScheduleType } from '../../generated';
 
 export const Schedule: TypeResolver<ScheduleType> = {
   gameId: root => root.gameId,
+  endTimeUTC: root => root.endTimeUTC,
+  hTeam: root => root.hTeam,
+  isGameActivated: root => root.isGameActivated,
+  nugget: root => root.nugget,
+  period: root => root.period,
+  seasonYear: root => root.seasonYear,
+  startTimeUTC: root => root.startTimeUTC,
+  startDateEastern: root => root.startDateEastern,
+  statusNum: root => root.statusNum,
+  vTeam: root => root.vTeam,
 }
