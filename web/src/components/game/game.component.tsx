dc9d6c32dd5698994b3547767f3e1183fc8a231f
Ricky Gonzales <gonzalrick@gmail.com>
2019-05-19 00:17:03 +1000

    New game details ui

--- web/src/components/game/game.component.tsx
+++ web/src/components/game/game.component.tsx
@@ -1,23 +1,50 @@
 import { RouteComponentProps } from '@reach/router';
 import React, { Component } from 'react';
-import { IoIosArrowBack } from 'react-icons/io';
-import { Button, Col, Container, Row } from 'reactstrap';
+import { IoMdArrowBack } from 'react-icons/io';
+import { Col, Container, Row, Card, CardBody, TabPane } from 'reactstrap';
 
 import './game.component.scss';
 import { GetGameComponent } from '../../generated/graphqlComponents';
 import { Boxscore } from '../boxscore/boxscore.component';
 import { GameStats } from '../gameStats/gameStats.component';
 import { Loading } from '../loading/loading.component';
+import TabContent from 'reactstrap/lib/TabContent';
 
 interface Props {
   date: string,
   gameId: string,
 }
 
-export class Game extends Component<RouteComponentProps<Props>> {
+interface State {
+  active: string;
+}
+
+export class Game extends Component<RouteComponentProps<Props>, State> {
+
+  state: State;
+
+  constructor(props: RouteComponentProps<Props>) {
+    super(props);
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      active: 'stats',
+    };
+  }
 
   back() {
     window.history.back();
+  }
+
+  toggle(tab: string) {
+    if (this.state.active !== tab) {
+      this.setState({
+        active: tab,
+      })
+    }
+  }
+
+  isActive(tab: string) {
+    return this.state.active === tab ? 'sectionLink active' : 'sectionLink ';
   }
 
   public render() {
@@ -28,16 +55,43 @@ export class Game extends Component<RouteComponentProps<Props>> {
           return (
             <div className="game">
               <Container>
-                <Button color="primary" size="sm" onClick={() => this.back()}>
-                  <IoIosArrowBack />
-                  Back
-                </Button>
-                <Row>
-                  <Col xs="12" sm="12" md="12" lg="12" className="gameItem">
-                    <Boxscore period={data.game.period} hTeam={data.game.hTeam} vTeam={data.game.vTeam} playoffs={data.game.playoffs}/>
-                    <GameStats gameData={data.game}/>
-                  </Col>
-                </Row>
+                <Card className="mb-2">
+                  <CardBody>
+                    <Row className="titleRow">
+                      <Col xs="1">
+                        <IoMdArrowBack className="backIcon" onClick={() => this.back()} />
+                      </Col>
+                      <Col className="title" xs={{ size: 8, offset: 1 }}>
+                        <span>{data.game.vTeam.fullName} at {data.game.hTeam.fullName}</span>
+                      </Col>
+                    </Row>
+
+                    <Row noGutters>
+                      <Col className={this.isActive('stats')} onClick={() => this.toggle('stats')} xs="4">
+                        <span>Stats</span>
+                      </Col>
+                      <Col className={this.isActive('boxscore')} onClick={() => this.toggle('boxscore')} xs="4">
+                        <span>Boxscore</span>
+                      </Col>
+                      <Col className={this.isActive('plays')} onClick={() => this.toggle('plays')} xs="4">
+                        <span>Play by play</span>
+                      </Col>
+                    </Row>
+                  </CardBody>
+                </Card>
+                <TabContent activeTab={this.state.active}>
+                  <TabPane tabId="stats">
+                    <GameStats gameData={data.game} />
+                  </TabPane>
+                  <TabPane tabId="boxscore">
+                    <Boxscore
+                      period={data.game.period}
+                      hTeam={data.game.hTeam}
+                      vTeam={data.game.vTeam}
+                      playoffs={data.game.playoffs}
+                    />
+                  </TabPane>
+                </TabContent>
               </Container>
             </div>
           );
